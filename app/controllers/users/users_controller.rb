81432524bd5a966a0833c93ede5a0bfeec5ca765
yunikonchak23 <uetamago.btssuga@gmail.com>
2021-07-11 05:04:39 +0000

    user途中

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -1,16 +1,18 @@
 class Users::UsersController < ApplicationController
   def show
+    @user = current_user
   end
-  
+
   def edit
+    @user = current_user
   end
-  
+
   def update
   end
-  
+
   def confirm
   end
-  
+
   def out
   end
 end

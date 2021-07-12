043ee9969f857b334bcad475996b055604765d72
yunikonchak23 <uetamago.btssuga@gmail.com>
2021-07-12 21:36:49 +0000

    ヘッダーとハンバーガーメニュー追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,21 @@
 //= require popper
 //= require bootstrap-sprockets
 
+//= require jquery
+//= require jquery_ujs
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+/*global $*/
+$(function() {
+    $('.hamburger').click(function() {
+        $(this).toggleClass('active');
+
+        if ($(this).hasClass('active')) {
+            $('.hm-Menu').addClass('active');
+        } else {
+            $('.hm-Menu').removeClass('active');
+        }
+    });
+});

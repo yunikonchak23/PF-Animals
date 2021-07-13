6d716728677cbfe8cfb6e0079d2c712941fbadfe
yunikonchak23 <uetamago.btssuga@gmail.com>
2021-07-13 18:40:34 +0000

    pet機能一応完成

--- app/controllers/users/pets_controller.rb
+++ app/controllers/users/pets_controller.rb
@@ -1,23 +1,30 @@
 class Users::PetsController < ApplicationController
   def index
-    @pet = Pet.new
+    @pet = current_user.pets.build
     @user = current_user
     @pets = Pet.all
   end
 
   def create
-    @pet = Pet.new(pet_params)
+    @pet = current_user.pets.build(pet_params)
     @pet.save
     redirect_back(fallback_location: root_path)
   end
 
   def edit
+    @pet = Pet.find(params[:id])
   end
 
   def update
+    @pet = Pet.find(params[:id])
+    @pet.update(pet_params)
+    redirect_to pets_path
   end
 
   def destroy
+    @pet = Pet.find(params[:id])
+    @pet.destroy
+    redirect_back(fallback_location: root_path)
   end
 
   private
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,5 @@
 class Pet < ApplicationRecord
+  belongs_to :user
   attachment :image
 
   enum gender: {未選択:0,男の子:1,女の子:2,性別不明:3}

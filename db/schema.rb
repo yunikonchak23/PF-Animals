b275293d50a9eedd92256c6e23967643fd810fe8
yunikonchak23 <uetamago.btssuga@gmail.com>
2021-07-17 21:03:18 +0000

    c

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_15_063031) do
+ActiveRecord::Schema.define(version: 2021_07_17_184807) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 2021_07_15_063031) do
     t.integer "user_id"
     t.integer "question_id"
     t.text "answer_body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "diary_id"
+    t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
